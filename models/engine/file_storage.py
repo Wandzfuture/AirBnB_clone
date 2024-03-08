e9aeb4e26d98cc7fe191bb25eb501e27066f69d1
Wandzfuture <michael.zabas@gmail.com>
2024-03-07 20:39:07 -0800

    0x00. AirBnB clone - The console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """Defines the class FileStorage."""
-from models.base_model import BaseModel
+
 import json
-import models
-from datetime import datetime
-from models.place import Place
+from models.base_model import BaseModel
+from models.user import User
 from models.state import State
 from models.city import City
-from models.user import User
+from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
 
 class FileStorage:
-    """A class for serializing and deserializing objects to a file."""
+    """A class for serializing and deserializing objects to a file>
 
+    Attributes:
+        __file_path (str): the name of the file to save objects to.
+        __objects (dict): a dictionary of instantiated objects.
+    """
     __file_path = "file.json"
     __objects = {}
 
@@ -37,18 +40,15 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    obj_dict = {}
-                    for k, v in value.items():
-                        if k == 'created_at' or k == 'updated_at':
-                            v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
-                        obj_dict[k] = v
-                    inst = eval(class_name)(**obj_dict)
-                    FileStorage.__objects[key] = inst
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for key, value in objdict.values():
+                    cls_name = value["__class__"]
+                    del value["__class__"]
+                    obj = globals()[cls_name](**value)
+                    key = "{}.{}".format(cls_name, value["id"])
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
-            pass
+            return
